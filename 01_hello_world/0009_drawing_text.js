709bdd5024ac9001d0f3dec97bb29fb2cf83b899
Sipondo <tt_robroek@hotmail.com>
2026-01-26 20:08:59 +0100

    larger text

--- 01_hello_world/0009_drawing_text.js
+++ 01_hello_world/0009_drawing_text.js
@@ -3,17 +3,17 @@ function setup() {
     background(220); // grey background
     
     // Set text properties
-    textSize(32); // big text
+    textSize(48); // big text
     fill(255, 0, 0); // red colour
     text("Hello", 50, 100);
     
     // Different size and colour
-    textSize(24); // medium text
+    textSize(36); // medium text
     fill(0, 255, 0); // green colour
     text("World!", 200, 150);
     
     // Another text example
-    textSize(16); // small text
+    textSize(24); // small text
     fill(0, 0, 255); // blue colour
     text("p5.js", 150, 250);
 }
