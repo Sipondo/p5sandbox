c17c1390ffa2ad05aeea694112ca84df875fd142
Sipondo <tt_robroek@hotmail.com>
2026-01-26 19:12:51 +0100

    canvas

---
+++ 01_hello_world/0000_canvas.js
@@ -0,0 +1,9 @@
+function setup() {
+    createCanvas(500, 500);
+}
+
+function draw() {
+    background(220); // grey background
+  	fill(255, 0, 0); // colour red
+    rect(50, 100, 200, 275); // rectangle from x=50 y=100 to x=200 y=275
+}
