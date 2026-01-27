c1eb70678ccea59bf8e620558487cb84f0b3e537
Ties Robroek <ties@trobroek.com>
2026-01-27 12:43:44 +0100

    fix issue with mouse js

--- 01_hello_world/0008_mouse.js
+++ 01_hello_world/0008_mouse.js
@@ -3,8 +3,7 @@ function setup() {
 }
 
 function draw() {
-    clear(); // Grey background each frame. What happens if we don't do this here?
-    background(220); // grey background
+    background(220); // Grey background each frame. What happens if we don't do this here?
     circle(mouseX, mouseY, 50); // Draw a circle at the mouse position
     rect(mouseX + 150, mouseY, 50, 50); // Draw a rectangle 150 pixels to the right of the mouse
 }
