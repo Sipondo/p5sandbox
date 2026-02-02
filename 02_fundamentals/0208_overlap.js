ba55d7b69fe3434014d097c5adf0ca38e38f3433
Ties Robroek <ties@trobroek.com>
2026-02-02 13:07:11 +0100

    overlap

---
+++ 02_fundamentals/0208_overlap.js
@@ -0,0 +1,21 @@
+function setup() {
+    createCanvas(400, 400);
+    background(220); // grey background
+    
+    // Set text properties
+    textSize(48); // big text
+    fill(255, 0, 0); // red colour
+    text("Hello", 50, 100);
+    
+    // Draw an ellipse
+    fill(0, 255, 0); // green colour
+    ellipse(50, 100, 120, 80);
+    
+    // Draw a rounded rectangle
+    fill(0, 0, 255); // blue colour
+    rect(50, 100, 200, 100, 20); // The 5th argument is *optional* and defines the corner radius
+}
+
+function draw() {
+    // Static drawing
+}
