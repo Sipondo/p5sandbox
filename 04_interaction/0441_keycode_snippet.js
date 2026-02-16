396a54da1b834159cdc0e78aad396252a638304f
Sipondo <tt_robroek@hotmail.com>
2026-02-16 19:43:27 +0100

    key snippet

--- 04_interaction/0441_keycode_snippet.js
+++ 04_interaction/0441_keycode_snippet.js
@@ -1,11 +1,14 @@
 function setup() {
   createCanvas(400, 400);
+  textSize(32);
+  text("Type something!", 50, 100);
 }
 
 function keyPressed() {
   // Run whenever there is a key press event
   clear()
-  text('"' + str(key) + '"', 50, 50);
-  text(str(keyCode), 50, 150);
+  text("Type something!", 50, 100);
+  text('"' + str(key) + '"', 50, 200);
+  text(str(keyCode), 50, 300);
   console.log(key, keyCode);
 }
