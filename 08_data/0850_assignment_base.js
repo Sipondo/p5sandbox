fc4c8482f711833d18e34a92889d30aa56d05ee1
Sipondo <tt_robroek@hotmail.com>
2026-03-16 20:28:33 +0100

    assignment

---
+++ 08_data/0850_assignment_base.js
@@ -0,0 +1,21 @@
+var MAP_URL = "https://rawcdn.githack.com/Sipondo/creative-programming-2026/57bf1d0b3f25b4baadde2c986a3f43a736b7afc2/lecture_8/media/denmark.jpg";
+let denmarkMap;
+
+function preload() {
+	// Load the map image before setup/draw starts
+	denmarkMap = loadImage(MAP_URL);
+
+	// TODO: load the city data
+}
+
+function setup(){
+	createCanvas(405, 488); // Correct ratio for image
+}
+
+function draw() {
+	// Draw map as the background
+	background(240);
+	image(denmarkMap, 0, 0, width, height);
+
+	// TODO: add some visualisation logic for cities
+}
