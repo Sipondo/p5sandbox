f38e7c56b9c107ecabfe689f3d602201ccdbafeb
Ties Robroek <ties@datologyai.com>
2026-03-23 17:33:58 +0100

    0901

--- 09_visualisation/0901_colored_bars.js
+++ 09_visualisation/0901_colored_bars.js
@@ -3,14 +3,16 @@ let values = [25, 55, 40, 70, 60, 85, 35];
 
 function setup() {
   createCanvas(500, 420);
+  textAlign(CENTER); // Centre text instead of left-aligning
+  noStroke();
 }
 
 function draw() {
   background(245);
 
-  fill(30);
+  fill("black");
   textSize(18);
-  text("Daily screen time (minutes)", 20, 30);
+  text("Daily screen time (minutes)", width / 2, 30);
 
   let startX = 30;
   let bottomY = 370;
@@ -22,24 +24,19 @@ function draw() {
     let x = startX + i * (barWidth + spacing);
     let barHeight = values[i] * scale;
 
-    // Map value to color: low = blue, high = red
+    // Map value to colour: low = blue, high = red
     let r = map(values[i], 25, 85, 60, 231);
     let g = map(values[i], 25, 85, 150, 76);
     let b = map(values[i], 25, 85, 220, 60);
 
     fill(r, g, b);
-    noStroke();
     rect(x, bottomY - barHeight, barWidth, barHeight, 4, 4, 0, 0);
 
     // Value label
-    fill(50);
+    fill("black");
     textSize(12);
-    textAlign(CENTER);
     text(values[i], x + barWidth / 2, bottomY - barHeight - 8);
-
     // Day label
-    fill(100);
-    textSize(11);
     text(labels[i], x + barWidth / 2, bottomY + 18);
   }
 
